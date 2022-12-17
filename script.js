3c97c991a2201f0d21df1f8b9383e4d644a63af2
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2022-12-17 00:35:48 -0500

    final

--- script.js
+++ script.js
@@ -1,63 +1,83 @@
+let numArray;
 function validateCard(card) {
   let multiple;
   let sum;
   let pushMultiple = [];
   let pushSum = [];
-  let numArray = Array.from(`${card}`);
+  numArray = (Array.from(`${card}`)).reverse();
   let over = [];
   let resultOne;
   let resultTwo;
   let less = [];
-  console.log(numArray);
+  console.log(numArray, "num");
   for (let i = 0; i < numArray.length; i++) {
     if (i % 2 !== 0) {
       multiple = numArray[i] * 2;
-      console.log(multiple, "multiple");
+
       pushMultiple.push(multiple);
     } else {
       sum = numArray[i] * 1;
-      console.log(sum, "sum");
+   
       pushSum.push(sum);
     }
   }
 
   let combo = pushMultiple.concat(pushSum);
-  //   console.log(combo, "combo");
 
   for (let i = 0; i < combo.length; i++) {
     if (combo[i] > 9) {
-      console.log(combo[i]);
+
       let lastNum = combo[i].toString();
 
       for (let j = 0; j < lastNum.length; j++) {
         over.push(+lastNum.charAt(j));
-        console.log(over, "over last");
 
         resultOne = over.reduce((a, b) => {
           return a + b;
         });
-
-        console.log(resultOne, "result");
       }
     } else {
   
       less.push(combo[i]);
-        console.log(less, "lessssss")
+  
       resultTwo = less.reduce((a, b) => {
         return a + b;
       });
 
-        console.log(resultTwo, "result 2");
     }
   }
-  let finalCard = resultOne + resultTwo
-  console.log(finalCard)
-  if(finalCard%10 === 0){
-    console.log("It is a valid card");
-  } else{
-    console.log("Invalid card!!!!")
-  }
+let finalCard;
+  if(isNaN(resultOne)){
+    finalCard = resultTwo;
+
+}
+else{
+   finalCard = resultOne + resultTwo
+  console.log(finalCard, "second if")
+
+}
+if(!isNaN(card)){
+    if(finalCard%10 === 0){
+        console.log("It is a valid card");
+        
+card.value = null;
+      } else{
+        console.log("Invalid card!!!!")
+      
+      }
+      console.log(finalCard)
+} else{
+  
+
 }
 
-validateCard(4222222222222);
 
+}
+
+
+// validateCard();
+
+validateCard(1234567890123456); //should be false
+validateCard(4408041234567893); //should be true
+validateCard(38520000023237); //should be true
+validateCard(4222222222222); //should be true
